d87b87283fa19206b919c909c9571661fe1020f4
lrb2333 <591392447@qq.com>
2017-03-07 00:56:40 +0800

    uptate: 排名完成 真的要睡觉了 = =

--- js/index.js
+++ js/index.js
@@ -2,5 +2,28 @@ var mySwiper = new Swiper('.swiper-container', {
     direction: 'vertical',
     loop: false,
     height: window.innerHeight,
-    initialSlide: 1
+    initialSlide: 1,
+    speed: 500,
 });
+
+
+function judgeWidth () {
+    var dpr = window.dpr;
+    var width = window.innerWidth;
+    var title = $('.about-me-title');
+
+    if (width <= 640) {
+        title.style.marginTop = '.5rem';
+    }
+    
+}
+
+function $ (ele) {
+    if (document.querySelectorAll(ele).length === 1) {
+        return document.querySelector(ele);
+    } else {
+        return document.querySelectorAll(ele);
+    }
+}
+
+judgeWidth();
