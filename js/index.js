0c3302848820f436e5ee5b7c443276c9531ebf6e
lrb2333 <591392447@qq.com>
2017-03-09 14:08:41 +0800

    update: 80%

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ var mySwiper = new Swiper('.swiper-container', {
     autoHeight: true,
     loop: false,
     height: window.innerHeight,
-    initialSlide: 4,
+    initialSlide: 0,
     speed: 500,
     iOSEdgeSwipeDetection : true,
     iOSEdgeSwipeThreshold : 30,
-    followFinger: false
+    followFinger: true
 });
 
 
@@ -16,11 +16,18 @@ function judgeWidth () {
     var width = window.innerWidth;
     var title = $('.about-me-title');
     var failMajor = $('.fail-major');
+    var major = $('.fail-major');
 
     if (width <= 640) {
         title[0].style.marginTop = '.5rem';
         title[1].style.marginTop = '.5rem';
         title[2].style.marginTop = '.5rem';
+        for (var i = 0; i < major.length; i++) {
+            if (major[i].innerHTML.length > 4) {
+                major[i].innerHTML = major[i].innerHTML.slice(0, 3) + '…';
+            }
+            console.log(major[i].innerHTML);
+        }
     }
     
     for (var i = 1; i < failMajor.length; i++) {
